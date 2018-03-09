5af87d05b7f319c15f15c78440a7fdfa65b96f32
xinglongjian <xinglongjian@qq.com>
2018-03-09 11:28:51 +0800

    jmx mbean monitor

--- xingtan-monitor/src/main/java/com/xingtan/monitor/jmx/TestStatisticsImpl.java
+++ xingtan-monitor/src/main/java/com/xingtan/monitor/jmx/TestStatisticsImpl.java
@@ -1,9 +1,22 @@
 package com.xingtan.monitor.jmx;
 
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
 
+@Component
+@ManagedResource(description = "Test Statistic",
+        objectName = "com.xingtan.monitor:name=test")
 public class TestStatisticsImpl implements TestStatistics {
     @Override
+    @ManagedAttribute(description = "Total", defaultValue = "0")
     public int getTotal() {
         return 10;
+    }
+
+    @ManagedOperation(description = "getMessage function")
+    public String getMessage() {
+        return "test mbean";
     }
 }
